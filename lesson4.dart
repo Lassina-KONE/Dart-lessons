efa9015efe668288a61bbff9127c5a804643e020
Lassina-KONE <58139655+Lassina-KONE@users.noreply.github.com>
2021-02-23 13:14:49 +0000

    Convert imperative code to functional

--- lesson4.dart
+++ lesson4.dart
@@ -2,7 +2,8 @@ String scream(int length) => "A${'a' * length}h!";
 
 main() {
   final values = [1, 2, 3, 5, 10, 50];
-  for (var length in values) {
-    print(scream(length));
-  }
+  print("############### Convert imperative code to functional #########################");
+  values.map(scream).forEach(print);
+  print("\n\n############### Use more Iterable features #########################");
+  values.skip(1).take(3).map(scream).forEach(print);
 }
